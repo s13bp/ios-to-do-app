76096bfa2c066fd8eb6fe3174580f990e9aa8643
Inga <ingabr1@hotmail.com>
2022-10-12 12:20:14 +0200

    Initial Commit

---
+++ ios-tasks-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ios-tasks-app
+//
+//  Created by Inga Brandsnes on 12/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
